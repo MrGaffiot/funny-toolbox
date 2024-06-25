9d1cf9054c099dd3ea2cfef1712fc8a7fe79d91c
MrWalper <MrWalper@proton.me>
2024-06-25 17:26:16 +0200

    Updated main.py so that it does the worky :3

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 import click
 import scriptFiles.imageSeparator as separator
 import os
+from colorama import Fore
 
 line1 = "      ___           ___           ___           ___           ___       ___     "
 line2 = "     /\  \         /\  \         /\  \         /\  \         /\__\     /\  \    "
@@ -15,17 +16,30 @@ line10 = "                                \::/  /       \::/  /       \:\__\    
 line11 = "                                 \/__/         \/__/         \/__/     \/__/    "
 ascii_art_lines = [line1, line2, line3, line4, line5, line6, line7, line8, line9, line10, line11]
 
+def cprint(text: str, size=os.get_terminal_size().columns):
+    print(text.center(size, " "))
+
 def start():
     size = os.get_terminal_size().columns
     for line in ascii_art_lines:
         print(line.center(size, " "))
+    cprint("Welcome to ftools (short for funny tools)! This is just a small project by me (walper),", size=size)
+    cprint("So don't expect anything crazy.", size=size)
+    cprint("List of features:", size=size)
+    cprint("separate: separates parts of images that are not connected into multimple images")
+    
+    command = input("> ")
 
+    if command == "separate":
+        separate()
+    else:
+        print(Fore.RED + "Unknown command!" + Fore.RESET)
+    
 @click.command()
-@click.option("--i", prompt="Path to the input image: ", help="The path for the image to process.")
-@click.option("--o", prompt="Path to folder for output: ", help="The path for the output image.")
+@click.option("--i", prompt="Path to the input image", help="The path for the image to process.")
+@click.option("--o", prompt="Path to folder for output", help="The path for the output image.")
 def separate(i, o):
     separator.separate_connected_components(image_path=i, output_dir=o)
 
 if __name__=="__main__":
     start()
-
